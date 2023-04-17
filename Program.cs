96464af836447bcd091805c053421120028a3d38
Михайлова Валерия Романовна <114227035+valerrron23@users.noreply.github.com>
2023-04-17 17:44:37 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using Telegram.Bot.Types.ReplyMarkups;
 
 internal class Program
 {
-    static ITelegramBotClient bot = new TelegramBotClient("****");
+    static ITelegramBotClient bot = new TelegramBotClient("5891816266:AAEUnFXyxwuHXz-xKws8pq8cgu-*******");
 
     public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
